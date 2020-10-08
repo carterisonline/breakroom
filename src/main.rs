0e3f9c7a23294492c71696e1f0b726ef43aec2f8
carterisonline <carter.reeb.developer@gmail.com>
2020-10-08 12:00:03 +0000

    Formatting for Printing Page #s

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,37 @@ extern crate markdown;
 use std::env;
 
 fn main() {
+    let mut temp_c: usize = 0;
+
     let args: Vec<String> = env::args().collect();
 
-    let options = fs_extra::dir::DirOptions {
-        depth: 3
-    };
-    let dir_content = fs_extra::dir::get_dir_content2(&args[1], &options).ok().unwrap();
+    let options = fs_extra::dir::DirOptions { depth: 3 };
+    let dir_content = fs_extra::dir::get_dir_content2(&args[1], &options)
+        .ok()
+        .unwrap();
     for filename in dir_content.files {
         if filename.ends_with(".md") {
             fs_extra::dir::create(&args[2], false);
-            fs_extra::file::write_all( &filename, markdown::to_html(fs_extra::file::read_to_string(&filename).ok().unwrap().as_str()).as_str());
+            fs_extra::file::write_all(
+                &filename,
+                fs_extra::file::read_to_string(&filename)
+                    .ok()
+                    .unwrap()
+                    .as_str()
+                    .split(' ')
+                    .enumerate()
+                    .map(|(c, s)| {
+                        temp_c = c;
+                        if s.starts_with("[[") {
+                            return format!("**PAGE {}**", &temp_c);
+                        }
+                        s.to_string()
+                    })
+                    .collect::<Vec<String>>()
+                    .into_iter()
+                    .collect::<String>()
+                    .as_str(),
+            );
         }
     }
 }
